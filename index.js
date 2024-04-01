635ac1a9760f3ef1dcef2409006b6f44c9b4e8fc
wGuerreroc333 <wguerreroc1969@gmail.com>
2024-04-01 13:38:37 -0500

    lLa ruta del post no deja enviar

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import  express  from "express"
+import morgan from "morgan"
 const app = express()
 
 app.get("/", (req,res)=>{res.send("RUTA OK")})
 
-app.get("/render", (req,res)=>{res.render("./index.ejs")})
+app.get("/render", (req,res)=>{res.render("./index.ejs", {variable: "Variable desde la ruta"})})
 
+app.post("/post", (req,res)=>{res.send("Post OK")})
 
-console.log("Heyyy")
-var port = process.env.PORT || 3000;
+app.post("/form", (req,res)=>{
+    const nombre = req.body.nombre;
+    const email = req.body.email;
+
+    console.log(nombre)
+
+})
+
+app.use(morgan('dev'))
+const port = process.env.PORT || 3000;
 
 app.listen(port)
 console.log("Escuchandome por puerto "+port)
